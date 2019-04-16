8d1ec058a96d263ba28438b49c01183843af1357
James Declerus <james.declerus@esih.edu>
2019-04-16 15:19:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,27 +17,18 @@ def summon_captain_planet(array)
   nu_array
 end
 
-summon_captain_planet(planeteer_calls)
-
-
 def long_planeteer_calls(array)
   i = 0
   if  array.any? {|i| i.length > 4}
     return true
   else
     return false
-  i = i + 1
+  i += 1
   end
 end
 
-long_planeteer_calls(call_screams)
 
 
-cheese_types = ["cheddar", "gouda", "camembert"]
-snacks = ["crackers", "gouda", "thyme"]
-
-
-# This is the learn.co method that wants the cheese array inside the actual method
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
@@ -45,7 +36,6 @@ def find_the_cheese(array)
   end
 end
 
-find_the_cheese(snacks)
 
 
 
